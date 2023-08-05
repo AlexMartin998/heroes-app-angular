ba77152a8be089b4c8eadca37e81d2dbd3ec6a2b
Alex Martin <alextravc.342@gmail.com>
2023-08-05 12:59:03 -0500

    [ADD]: heroes routing added

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeroLayoutComponent } from './layouts/hero-layout/hero-layout.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HeroLayoutComponent,
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+
+      // at the end, 'cause it matches all routes
+      { path: ':id', component: HeroPageComponent }, // domain.tld/heroes/:id
+      { path: '**', redirectTo: 'list' }, //   domain.tld/heroes -> /list
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
