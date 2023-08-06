ad6dd1c39273b21311f8938107bebb12934aa837
Alex Martin <alextravc.342@gmail.com>
2023-08-06 09:05:23 -0500

    [ADD]: heroImage custom pipe added

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
+import { CardComponent } from './componets/card/card.component';
 import { HeroLayoutComponent } from './layouts/hero-layout/hero-layout.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { CardComponent } from './componets/card/card.component';
+
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,7 @@ import { CardComponent } from './componets/card/card.component';
     SearchPageComponent,
     HeroLayoutComponent,
     CardComponent,
+    HeroImagePipe,
   ],
   imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
