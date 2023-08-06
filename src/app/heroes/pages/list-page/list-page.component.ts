8481a9ae096556729bd73fdf359b3c2f72198d37
Alex Martin <alextravc.342@gmail.com>
2023-08-05 22:24:12 -0500

    [ADD]: fetch heroes data in service added

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'heroes-list-page',
   templateUrl: './list-page.component.html',
 })
-export class ListPageComponent {}
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = [];
+
+  constructor(private readonly heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    // toda req http se dispara cuando se monta el component, como es 1 Observable .subscribe()
+    this.heroesService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+}
