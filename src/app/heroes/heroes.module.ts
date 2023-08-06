46d2404cad000c2173b96b425983408c84e5dcf6
Alex Martin <alextravc.342@gmail.com>
2023-08-05 22:56:30 -0500

    [ADD]: heroe card component + assets (without imagePipe)

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './componets/card/card.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     NewPageComponent,
     SearchPageComponent,
     HeroLayoutComponent,
+    CardComponent,
   ],
   imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
